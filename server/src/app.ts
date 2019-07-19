6b6b9ab3606a27d6ab406e564d3a566048a8399c
Toke Reimer Skovgaard <tors@sundhed.dk>
2019-07-19 11:11:58 +0200

    Fixed history

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import history from 'connect-history-api-fallback';
+
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors());
-app.use(express.static('../frontend/dist'))
+app.use(history());
+app.use(express.static('../frontend/dist'));
 
-// const port = process.env.PORT || 4000;
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
